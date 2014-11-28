1faa7361c2be2953d4cbe08216cc754913187cf5
gqshao <sgq0085@163.com>
2014-11-28 15:30:50 +0800

    添加对RMI学习了解

---
+++ learn-core-java/src/main/java/com/gqshao/rmi/IHello.java
@@ -0,0 +1,24 @@
+package com.gqshao.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * 定义一个远程接口，必须继承Remote接口，其中需要远程调用的方法必须抛出RemoteException异常
+ */
+public interface IHello extends Remote {
+    /**
+     * 简单的返回“Hello World！"字样
+     * @return 返回“Hello World！"字样
+     * @throws java.rmi.RemoteException
+     */
+    public String helloWorld() throws RemoteException;
+
+    /**
+     * 一个简单的业务方法，根据传入的人名返回相应的问候语
+     * @param someBodyName  人名
+     * @return 返回相应的问候语
+     * @throws java.rmi.RemoteException
+     */
+    public String sayHelloToSomeBody(String someBodyName) throws RemoteException;
+}
---
+++ learn-core-java/src/test/java/com/gqshao/rmi/HelloServerTest.java
@@ -0,0 +1,31 @@
+package com.gqshao.rmi;
+
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+/**
+ * 客户端测试，在客户端调用远程对象上的远程方法，并返回结果。
+ */
+public class HelloServerTest {
+
+    @Test
+    public void testRmi() {
+        try {
+            //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法
+            IHello rhello = (IHello) Naming.lookup("rmi://localhost:8888/RHello");
+            System.out.println(rhello.helloWorld());
+            System.out.println(rhello.sayHelloToSomeBody("世界!"));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
